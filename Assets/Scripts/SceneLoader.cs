9bb54802a7a461bfd71da609ac66ea84d7cf630c
bendezu <benezuvlad@gmail.com>
2021-11-07 20:14:01 +0300

    Implement routing to game scene on play button click

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader: MonoBehaviour
+{
+
+    public static void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
